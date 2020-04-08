d97d6a0e2db35bfd229e46277a2f5f931a71753e
Jon B <jon.burger@sage.com>
2020-04-08 14:30:12 +0100

    Update 11ty.js

--- bin/11ty.js
+++ bin/11ty.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+console.log('Yay 11ty!');
+
 const { spawn } = require('child_process');
 
-let watcher = spawn('npm',['run','build']);
+let watcher = spawn('npm', ['run', 'build']);
 
 watcher.stdout.on('data', (data) => {
 	console.log(`${data}`);

b723080f2c4822fbe6867bf64f86acec388134df
Jon B <jon.burger@sage.com>
2020-04-08 15:15:17 +0100

    Update 11ty.js

--- bin/11ty.js
+++ bin/11ty.js
@@ -2,8 +2,8 @@
 
 const { spawn, exec } = require('child_process');
 
-let watcher = spawn('npx', ['@11ty/eleventy', '--serve']);
+let watcher = spawn('npx', ['@11ty/eleventy', '--serve'], {stdio: [process.stdin, process.stdout, process.stderr]});
 
-watcher.stdout.on('data', data => {
-	console.log(data.toString());
-});
+// watcher.stdout.on('data', data => {
+// 	console.log(data.toString());
+// });

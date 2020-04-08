a03c225ec65b0b38749d5f26430765e4a67c7cf3
Jon B <jon.burger@sage.com>
2020-04-08 14:57:56 +0100

    Update 11ty.js

--- bin/11ty.js
+++ bin/11ty.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 const { spawn } = require('child_process');
+const cwd = process.cwd();
 
-let watcher = spawn('npm', ['run-script', 'serve'], {
-	cwd: process.cwd()
-});
+console.log('cwd', cwd);
+
+let watcher = spawn('npm', ['run-script', 'serve'], { cwd });
 
 watcher.stdout.on('data', (data) => {
 	console.log(`${data}`);

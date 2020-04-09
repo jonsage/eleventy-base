2c275081a53a35ae4e772135d69f6b1e98006e32
Jon B <jon.burger@sage.com>
2020-04-09 13:52:37 +0100

    Update 11ty.js

--- bin/11ty.js
+++ bin/11ty.js
@@ -3,5 +3,5 @@
 const { spawn } = require('child_process');
 const path = require('path');
 
-// spawn('npx', ['@11ty/eleventy', '--serve'], {stdio: [process.stdin, process.stdout, process.stderr]});
-spawn('npm', ['run', 'serve'], {cwd: path.join(__dirname, '..'), stdio: [process.stdin, process.stdout, process.stderr]});
+spawn('npx', ['@11ty/eleventy', '--serve'], {stdio: [process.stdin, process.stdout, process.stderr]});
+// spawn('npm', ['run', 'serve'], {cwd: path.join(__dirname, '..'), stdio: [process.stdin, process.stdout, process.stderr]});
